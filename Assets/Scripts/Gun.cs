098a10f9d00cbf5c2156775b9ac9a1c161a1c740
Bennet <Meier>
2021-11-03 10:12:30 +0100

    Comments

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -17,12 +17,18 @@ public class Gun : MonoBehaviour
 
     private void shoot()
     {
+        /*Screen to world Point wandelt screen space (UI) ind world space (spiel) um.
+         Mausposition ist teil der Input klasse.
+         transform.position kann immer abgefragt werden, da wir von monobehaviour erben
+         wir zielen also in die Richtung, die zwischen unserer Position und der Mausposition aufgespannt wird*/
+        Vector2 aimAt = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+
+        /*Wir schreiben das neue Objekt auf eine lokale variable, damit wir wie hier z.B. den Bullet Component bekommen*/
         GameObject currentBullet = Instantiate(BulletObject, transform.position, Quaternion.identity);
 
         Bullet bullet = currentBullet.GetComponent<Bullet>();
 
-        Vector2 aimAt = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
-
+        // Der Vektor muss noch normalisiert werden, damit er immer den gleichen Betrag hat
         bullet.Direction = aimAt.normalized;
     }
 }
